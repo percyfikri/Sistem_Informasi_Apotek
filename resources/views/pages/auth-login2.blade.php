db77f68670644bf0a3b4020a8f65e2471bef7ab4
lenmoru666 <falendikapratama@gmail.com>
2023-03-20 16:37:08 +0700

    Tampilan Halaman Login

--- resources/views/pages/auth-login2.blade.php
+++ resources/views/pages/auth-login2.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
         name="viewport">
-    <title>Login &mdash; Stisla</title>
+    <title>Login &mdash; Apotek Arema</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet"
@@ -37,10 +37,9 @@
                             alt="logo"
                             width="80"
                             class="shadow-light rounded-circle mb-5 mt-2">
-                        <h4 class="text-dark font-weight-normal">Welcome to <span class="font-weight-bold">Stisla</span>
+                        <h4 class="text-dark font-weight-normal">Halaman <span class="font-weight-bold">Login</span>
                         </h4>
-                        <p class="text-muted">Before you get started, you must login or register if you don't already
-                            have an account.</p>
+                        <p class="text-muted"></p>
                         <form method="POST"
                             action="#"
                             class="needs-validation"
@@ -55,7 +54,7 @@
                                     required
                                     autofocus>
                                 <div class="invalid-feedback">
-                                    Please fill in your email
+                                    Mohon masukkan email anda!
                                 </div>
                             </div>
 
@@ -71,11 +70,11 @@
                                     tabindex="2"
                                     required>
                                 <div class="invalid-feedback">
-                                    please fill in your password
+                                    Mohon masukkan password anda!
                                 </div>
                             </div>
 
-                            <div class="form-group">
+                            {{-- <div class="form-group">
                                 <div class="custom-control custom-checkbox">
                                     <input type="checkbox"
                                         name="remember"
@@ -85,13 +84,13 @@
                                     <label class="custom-control-label"
                                         for="remember-me">Remember Me</label>
                                 </div>
-                            </div>
+                            </div> --}}
 
                             <div class="form-group text-right">
-                                <a href="auth-forgot-password.html"
+                                {{-- <a href="auth-forgot-password.html"
                                     class="float-left mt-3">
                                     Forgot Password?
-                                </a>
+                                </a> --}}
                                 <button type="submit"
                                     class="btn btn-primary btn-lg btn-icon icon-right"
                                     tabindex="4">
@@ -99,13 +98,10 @@
                                 </button>
                             </div>
 
-                            <div class="mt-5 text-center">
-                                Don't have an account? <a href="auth-register.html">Create new one</a>
-                            </div>
                         </form>
 
                         <div class="text-small mt-5 text-center">
-                            Copyright &copy; Your Company. Made with 💙 by Stisla
+
                             <div class="mt-2">
                                 <a href="#">Privacy Policy</a>
                                 <div class="bullet"></div>
@@ -119,15 +115,15 @@
                     <div class="absolute-bottom-left index-2">
                         <div class="text-light p-5 pb-2">
                             <div class="mb-5 pb-3">
-                                <h1 class="display-4 font-weight-bold mb-2">Good Morning</h1>
-                                <h5 class="font-weight-normal text-muted-transparent">Bali, Indonesia</h5>
+                                <h1 class="display-4 font-weight-bold mb-2">Kesehatan Nomor Satu</h1>
+                                <h5 class="font-weight-normal text-muted-transparent">Jagalah kesehatan kalian!</h5>
                             </div>
-                            Photo by <a class="text-light bb"
+                            {{-- Photo by <a class="text-light bb"
                                 target="_blank"
-                                href="https://unsplash.com/photos/a8lTjWJJgLA">Justin Kauffman</a> on <a
+                                href="https://unsplash.com/photos/a8lTjWJJgLA">Len Kurniawan</a> on <a
                                 class="text-light bb"
                                 target="_blank"
-                                href="https://unsplash.com">Unsplash</a>
+                                href="https://unsplash.com">Unsplash</a> --}}
                         </div>
                     </div>
                 </div>
--- resources/views/pages/auth-register.blade.php
+++ resources/views/pages/auth-register.blade.php
@@ -11,26 +11,18 @@
 @section('main')
     <div class="card card-primary">
         <div class="card-header">
-            <h4>Register</h4>
+            <h4>Halaman Register</h4>
         </div>
 
         <div class="card-body">
             <form method="POST">
-                <div class="row">
-                    <div class="form-group col-6">
-                        <label for="frist_name">First Name</label>
-                        <input id="frist_name"
-                            type="text"
-                            class="form-control"
-                            name="frist_name"
-                            autofocus>
-                    </div>
-                    <div class="form-group col-6">
-                        <label for="last_name">Last Name</label>
-                        <input id="last_name"
-                            type="text"
-                            class="form-control"
-                            name="last_name">
+                <div class="form-group">
+                    <label for="email">Nama Lengkap</label>
+                    <input id="nama"
+                        type="text"
+                        class="form-control"
+                        name="nama">
+                    <div class="invalid-feedback">
                     </div>
                 </div>
 
@@ -61,7 +53,7 @@
                     </div>
                     <div class="form-group col-6">
                         <label for="password2"
-                            class="d-block">Password Confirmation</label>
+                            class="d-block">Konfirmasi Password</label>
                         <input id="password2"
                             type="password"
                             class="form-control"
