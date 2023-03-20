1f06def0421b8eeb2d3de23f2fe16544e96856c9
AdamPoi <isadamrr@gmail.com>
2023-03-20 16:16:25 +0700

    feat(migration): add resep obat & detail resep

---
+++ database/migrations/2023_03_20_090531_create_resep_obat_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('resep_obat', function (Blueprint $table) {
+      $table->increments('id_resep');
+      $table->unsignedInteger('id_dokter');
+      $table->foreign('id_dokter')->references('id_pengguna')->on('pengguna')->onDelete('cascade');
+      $table->unsignedInteger('id_customer');
+      $table->foreign('id_customer')->references('id_pengguna')->on('pengguna')->onDelete('cascade');
+      $table->string('nama_resep');
+      $table->string('deskripsi');
+      $table->date('tanggal');
+      $table->string('status');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('resep_obat');
+  }
+};
