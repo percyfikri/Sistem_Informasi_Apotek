3ecf48d1dc497fc0db84d81e6d8a49717b28931d
AdamPoi <isadamrr@gmail.com>
2023-03-20 15:40:45 +0700

    feat(migration): add obat & stok obat migration

---
+++ database/migrations/2023_03_20_080948_create_obat_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('obat', function (Blueprint $table) {
+      $table->increments('id_obat');
+      $table->string('nama_obat');
+      $table->string('jenis_obat');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('obat');
+  }
+};
---
+++ database/migrations/2023_03_20_081004_create_stok_obat_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('stok_obat', function (Blueprint $table) {
+      $table->unsignedInteger('id_obat');
+      $table->foreign('id_obat')->references('id_obat')->on('obat')->onDelete('cascade');
+      $table->integer('statuan');
+      $table->integer('kuantitas');
+      $table->decimal('harga', 12, 2);
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('stok_obat');
+  }
+};
