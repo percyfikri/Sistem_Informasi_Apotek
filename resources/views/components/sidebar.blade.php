adee9e725aae1ae8a490ca275c784c3110112eea
AdamPoi <isadamrr@gmail.com>
2023-03-24 13:55:16 +0700

    fix(sidebar): add menu on sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -9,35 +9,49 @@
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
             <li class="nav-item dropdown {{ $type_menu === 'dashboard' ? 'active' : '' }}">
-                <a href="#"
-                    class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
                 <ul class="dropdown-menu">
                     <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
-                        <a class="nav-link"
-                            href="{{ url('dashboard-general-dashboard') }}">General Dashboard</a>
+                        <a class="nav-link" href="{{ url('dashboard-general-dashboard') }}">General Dashboard</a>
                     </li>
                     <li class="{{ Request::is('dashboard-ecommerce-dashboard') ? 'active' : '' }}">
-                        <a class="nav-link"
-                            href="{{ url('dashboard-ecommerce-dashboard') }}">Ecommerce Dashboard</a>
+                        <a class="nav-link" href="{{ url('dashboard-ecommerce-dashboard') }}">Ecommerce Dashboard</a>
                     </li>
                 </ul>
             </li>
             <li class="menu-header">Apoteker</li>
-            <li class="nav-item dropdown {{ $type_menu === 'dashboard' ? 'active' : '' }}">
-                <a class="nav-link"
-                    href="{{ url('obat-page') }}"><i class="far fa-file-alt"></i> <span>Obat</span></a>
+            <li class="nav-item dropdown {{ $type_menu === 'kasir' ? 'active' : '' }}">
+                <a class="nav-link" href="{{ url('kasir') }}"><i class="fa-solid fa-cash-register"></i>
+                    <span>Kasir</span></a>
             </li>
-            <li class="nav-item dropdown {{ $type_menu === 'dashboard' ? 'active' : '' }}">
-                <a class="nav-link"
-                    href="{{ url('blank-page') }}"><i class="far fa-file-alt"></i> <span>Resep Obat</span></a>
+            <li class="nav-item dropdown {{ $type_menu === 'penjualan' ? 'active' : '' }}">
+                <a class="nav-link" href="{{ url('penjualan') }}"><i class="fa-solid fa-money-bill-wave"></i>
+                    <span>Penjualan</span></a>
             </li>
-            <li class="nav-item dropdown {{ $type_menu === 'dashboard' ? 'active' : '' }}">
-                <a class="nav-link"
-                    href="{{ url('blank-page') }}"><i class="far fa-file-alt"></i> <span>Racikan</span></a>
+            <li class="nav-item dropdown {{ $type_menu === 'obat' ? 'active' : '' }}">
+                <a class="nav-link" href="{{ url('obat-page') }}"><i class="fa-solid fa-pills"></i>
+                    <span>Obat</span></a>
             </li>
-            <li class="nav-item dropdown {{ $type_menu === 'dashboard' ? 'active' : '' }}">
-                <a class="nav-link"
-                    href="{{ url('blank-page') }}"><i class="far fa-file-alt"></i> <span>Resep Racikan</span></a>
+            <li class="nav-item dropdown {{ $type_menu === 'resep-obat' ? 'active' : '' }}">
+                <a class="nav-link" href="{{ url('resep-obat') }}"><i class="fa-solid fa-prescription"></i>
+                    <span>Resep
+                        Obat</span></a>
+            </li>
+            <li class="nav-item dropdown {{ $type_menu === 'racikan' ? 'active' : '' }}">
+                <a class="nav-link" href="{{ url('racikan') }}"><i class="fa-solid fa-prescription-bottle"></i>
+                    <span>Racikan</span></a>
+            </li>
+            <li class="nav-item dropdown {{ $type_menu === 'customer' ? 'active' : '' }}">
+                <a class="nav-link" href="{{ url('customer') }}"><i class="fa-solid fa-address-book"></i>
+                    <span>Customer</span></a>
+            </li>
+            <li class="nav-item dropdown {{ $type_menu === 'dokter' ? 'active' : '' }}">
+                <a class="nav-link" href="{{ url('dokter') }}"><i class="fa-solid fa-user-doctor"></i>
+                    <span>Dokter</span></a>
+            </li>
+            <li class="nav-item dropdown {{ $type_menu === 'jasa' ? 'active' : '' }}">
+                <a class="nav-link" href="{{ url('jasa') }}"><i class="fa-solid fa-user-nurse"></i>
+                    <span>Jasa</span></a>
             </li>
     </aside>
 </div>
