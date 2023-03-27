2f7d3143469ff7466e5fe26f5adf2d595fe06d0b
abiyasa05 <abiyasap24@gmail.com>
2023-03-27 18:22:21 +0700

    Tambahan button dalam form edit obat

--- resources/views/pages/obat/edit.blade.php
+++ resources/views/pages/obat/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Halaman Edit Obat')
+@section('title', 'Edit Obat')
 
 @push('style')
     <link rel="stylesheet" href="{{ asset('library/select2/dist/css/select2.min.css') }}">
@@ -10,50 +10,41 @@
     <div class="main-content">
         <section class="section">
             <div class="section-header">
-                <h1>Detail Obat</h1>
+                <h1>Edit Obat</h1>
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="{{ url('dashboard-general-dashboard') }}">Dashboard</a></div>
                     <div class="breadcrumb-item active"><a href="{{ route('obat.index') }}">Obat</a></div>
-                    <div class="breadcrumb-item">Detail Obat</div>
+                    <div class="breadcrumb-item">Edit Obat</div>
                 </div>
             </div>
 
             <div class="section-body">
-                @if ($errors->any())
-                <div class="pt-3">
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        <ul>
-                        @foreach ($errors->all() as $item)
-                            <li>{{ $item }}</li>
-                        @endforeach
-                        </ul>
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                </div>
-                @endif
                 <div class="card">
                     <div class="card-header">
-                        <h4>Tambah Obat</h4>
-
+                        <h4>{{ $obat->nama_obat }}</h4>
+                        <div class="card-header-action">
+                            <button class="btn btn-danger btn-icon icon-left"
+                                data-action="{{ route('obat.destroy', $obat->id_obat) }}" data-toggle="modal"
+                                data-target="#confirm-delete-modal"> <i class="fas fa-trash"></i>
+                                Delete</button>
+                        </div>
                     </div>
                     <form action="{{ route('obat.update', $obat->id_obat) }}" method="POST">
                         <div class="card-body">
                             @csrf
                             @method('PUT')
                             <div class="form-group">
-                                <label>Nama obat</label>
+                                <label>Nama Obat</label>
                                 <input type="text" name="nama_obat"
-                                    class="form-control @if (old('nama_obat')) is-valid @endif 
-                                @error('nama_obat') is-invalid @enderror"
+                                    class="form-control @if (old('nama_obat')) is-valid @endif
+                                     @error('nama_obat') is-invalid @enderror"
                                     value="{{ old('nama_obat', $obat->nama_obat) }}">
                             </div>
                             <div class="form-group">
-                                <label>Jenis obat</label>
+                                <label>Jenis Obat</label>
                                 <input type="text" name="jenis_obat"
-                                    class="form-control @if (old('jenis_obat')) is-valid @endif 
-                                @error('jenis_obat') is-invalid @enderror"
+                                    class="form-control @if (old('jenis_obat')) is-valid @endif
+                                     @error('jenis_obat') is-invalid @enderror"
                                     value="{{ old('jenis_obat', $obat->jenis_obat) }}">
                             </div>
                         </div>
@@ -65,11 +56,11 @@
                 </div>
             </div>
         </section>
-
+        <x-modal.confirm-delete />
     </div>
 @endsection
 
-{{-- @push('scripts')
+@push('scripts')
     <script src="{{ asset('library/cleave.js/dist/cleave.min.js') }}"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="{{ asset('library/select2/dist/js/select2.full.min.js') }}"></script>
@@ -103,4 +94,4 @@
             numeralThousandsGroupStyle: 'thousand'
         });
     </script>
-@endpush --}}
+@endpush
