6a62604efc12a93c05076154e2e2eabc715b7fe2
abiyasa05 <abiyasap24@gmail.com>
2023-04-03 13:53:10 +0700

    Revisi Migration Stok Obat

--- database/migrations/2023_03_20_081004_create_stok_obat_table.php
+++ database/migrations/2023_03_20_081004_create_stok_obat_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
     Schema::create('stok_obat', function (Blueprint $table) {
       $table->unsignedInteger('id_obat');
       $table->foreign('id_obat')->references('id_obat')->on('obat')->onDelete('cascade');
-      $table->integer('statuan');
+      $table->string('satuan');
       $table->integer('kuantitas');
       $table->decimal('harga', 12, 2);
       $table->timestamps();
