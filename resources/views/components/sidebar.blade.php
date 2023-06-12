c8013f7f4f97ad0d4191fd78cd6bb8e35bb5b74a
abiyasa05 <abiyasap24@gmail.com>
2023-06-12 15:29:39 +0700

    Fix sidebar dashboard

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -12,7 +12,7 @@
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
                 <ul class="dropdown-menu">
                     <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
-                        <a class="nav-link" href="{{ url('dashboard-general-dashboard') }}">General Dashboard</a>
+                        <a class="nav-link" href="{{ url('dashboard') }}">General Dashboard</a>
                     </li>
                     <li class="{{ Request::is('dashboard-ecommerce-dashboard') ? 'active' : '' }}">
                         <a class="nav-link" href="{{ url('dashboard-ecommerce-dashboard') }}">Ecommerce Dashboard</a>
